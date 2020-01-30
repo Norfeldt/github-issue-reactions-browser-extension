ac512d222c2ab183afc5f9fdc222e9bcf23b4752
Lasse Norfeldt <ln@abtion.com>
2020-01-30 08:39:35 +0100

    Fix to now observe for mutations instead of the monkey hack

--- addReactionsNav.js
+++ addReactionsNav.js
@@ -1,32 +1,47 @@
 const URL =
-  window.location.origin + window.location.pathname + window.location.search
-const header = document.querySelector('#partial-discussion-sidebar')
-header.style = `position: relative;height: 100%;`
-let wrapper = getWrapper()
+  window.location.origin + window.location.pathname + window.location.search;
+const header = document.querySelector("#partial-discussion-sidebar");
+header.style = `position: relative;height: 100%;`;
+let wrapper = getWrapper();
 
-// The isolated world made it difficult to detect DOM changes in the shared DOM
-// So this monkey-hack to make it refresh when ..
-setInterval(() => {
-  wrapper.remove()
-  wrapper = getWrapper()
-  addReactionNav()
-}, 1000)
+addReactionNav(wrapper);
+
+// Select the node that will be observed for mutations.
+const targetNode = document.querySelector('body');
+
+// Options for the observer (which mutations to observe).
+const config = {
+  childList: true,
+  subtree: true
+};
+
+// Create an observer instance linked to the callback function.
+const observer = new MutationObserver(mutations => {
+  if (!targetNode.contains(wrapper) || mutations.some(mutation => mutation.target.matches('.js-timeline-item'))) {
+    wrapper.remove();
+    wrapper = getWrapper();
+    addReactionNav();
+  }
+});
+
+// Start observing the target node for configured mutations.
+observer.observe(targetNode, config);
 
 function getWrapper() {
-  const header = document.querySelector('#partial-discussion-sidebar')
-  const wrapper = header.appendChild(document.createElement('div'))
+  const header = document.querySelector("#partial-discussion-sidebar");
+  const wrapper = header.appendChild(document.createElement("div"));
   wrapper.style = `
       position:sticky;
       position: -webkit-sticky;
-      top:10px;`
-  return wrapper
+      top:10px;`;
+  return wrapper;
 }
 
 function addReactionNav() {
-  const title = document.createElement('div')
-  title.style = `font-weight: bold`
-  title.appendChild(document.createTextNode('Reactions'))
-  wrapper.appendChild(title)
+  const title = document.createElement("div");
+  title.style = `font-weight: bold`;
+  title.appendChild(document.createTextNode("Reactions"));
+  wrapper.appendChild(title);
 
   // Grabbing all reactions Reactions 👍 🚀 🎉 😄 ❤️ 😕 👎 👀
   const reactionsNodes = document.querySelectorAll(`
@@ -38,38 +53,38 @@ function addReactionNav() {
     [alias="thinking_face"].mr-1,
     [alias="-1"].mr-1,
     [alias="eyes"].mr-1
-  `)
+  `);
 
   const reactionsNodesParents = [
     ...new Set(
       Array.from(reactionsNodes).map(node => node.parentElement.parentElement)
-    ),
-  ]
+    )
+  ];
 
   reactionsNodesParents.forEach(node => {
-    const a = document.createElement('a')
-    const linkText = document.createTextNode('\n' + node.innerText)
-    a.appendChild(linkText)
-    a.title = node.innerText
+    const a = document.createElement("a");
+    const linkText = document.createTextNode("\n" + node.innerText);
+    a.appendChild(linkText);
+    a.title = node.innerText;
 
-    let id = null
+    let id = null;
     while (id == null || node != null) {
-      if (node.tagName === 'A' && node.name) {
-        id = node.name
-        break
+      if (node.tagName === "A" && node.name) {
+        id = node.name;
+        break;
       }
 
       if (node.id) {
-        id = node.id
-        break
+        id = node.id;
+        break;
       }
 
-      node = node.parentNode
+      node = node.parentNode;
     }
-    const postURL = URL + '#' + id
-    a.href = postURL
-    a.style = `display:block;`
+    const postURL = URL + "#" + id;
+    a.href = postURL;
+    a.style = `display:block;`;
 
-    wrapper.appendChild(a)
-  })
+    wrapper.appendChild(a);
+  });
 }
