5e6bde5c92051ceb3e3f1e0fde463bd0f2b4f67e
Lasse Nørfeldt <lasse@norfeldt.com>
2023-01-18 21:43:10 +0100

    ✨ 💄 add toggle between block and inline-block style

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
-console.log('running github issue reactions', Math.random())
+const wesBosYellow = '#ffc600'
+const cobalt2BackgroundBlue = '#183348'
+const DISPLAY = 'DISPLAY'
+
+const getBrowser = () => {
+  if (navigator.userAgent.match(/Chrome/)) return chrome
+
+  return browser
+}
 
 const [sideBarId, wrapperId] = [
   '#partial-discussion-sidebar',
@@ -10,7 +18,7 @@ function debounce(func: Function, timeout = 2000): Function {
   return (...args: any[]) => {
     clearTimeout(timer)
     timer = setTimeout(() => {
-      // @ts-expect-error
+      // @ts-ignore
       func.apply(this, args)
     }, timeout)
   }
@@ -21,7 +29,6 @@ injectWrapper({ withLoadingSpinner: true })
 
 // CHANGE PAGE
 const mainObserver = new MutationObserver((mutations) => {
-  // console.log('mainObserver')
   debounce(() => {
     startObservingComments()
     addReactionNav()
@@ -37,7 +44,6 @@ if (layoutMain !== null) {
 }
 
 function startObservingComments() {
-  // console.log('startObservingComments')
   const commentSection =
     document.querySelector('.Layout-main') ?? // Issues & PR
     document.querySelector('.js-discussion-quote-selection') // Discussion
@@ -54,7 +60,6 @@ function startObservingComments() {
 
 // Create a sticking wrapper to place all reactions
 function injectWrapper({ withLoadingSpinner } = { withLoadingSpinner: false }) {
-  console.log('injectWrapper')
   const header = document.querySelector(sideBarId) as HTMLDivElement
   if (!header) return
   header.style.position = 'relative'
@@ -79,10 +84,11 @@ function injectWrapper({ withLoadingSpinner } = { withLoadingSpinner: false }) {
 
 function LoadingSpinner() {
   const loadingSpinner = document.createElement('div')
-  loadingSpinner.style.width = '50px'
-  loadingSpinner.style.height = '50px'
-  loadingSpinner.style.border = '2px solid #f3f3f3'
-  loadingSpinner.style.borderTop = '2px solid #3498db'
+  const side = '25px'
+  loadingSpinner.style.width = side
+  loadingSpinner.style.height = side
+  loadingSpinner.style.border = `2px solid ${cobalt2BackgroundBlue}`
+  loadingSpinner.style.borderTop = `2px solid ${wesBosYellow}`
   loadingSpinner.style.borderRadius = '50%'
   loadingSpinner.style.animation = 'spin 1s linear infinite'
   const style = document.createElement('style')
@@ -99,7 +105,6 @@ function LoadingSpinner() {
 
 // Scan the site for reactions and stick it into the wrapper
 function addReactionNav() {
-  // console.log('addReactionNav')
   document.querySelector(wrapperId)?.remove()
   injectWrapper()
   const wrapper = document.querySelector(wrapperId)
@@ -116,23 +121,34 @@ function addReactionNav() {
 }
 
 function Title(title: string) {
-  // console.log('Title')
-  const element = document.createElement('div')
-  element.style.setProperty('font-weight', 'bold')
-  element.style.setProperty('margin', '1.25rem 0 0.5rem 0')
+  const element = document.createElement('div') satisfies HTMLDivElement
+  element.style.display = 'flex'
+  element.style.justifyContent = 'space-between'
+  element.style.alignItems = 'center'
+  element.style.fontWeight = 'bold'
+  element.style.margin = '1.25rem 0 0.5rem 0'
+
   element.appendChild(document.createTextNode(title))
+
+  const switchDiv = document.createElement('div')
+  switchDiv.appendChild(SvgBlockIcon())
+  switchDiv.appendChild(Switch())
+  switchDiv.appendChild(SvgInlineBlockIcon())
+  element.appendChild(switchDiv)
 
   return element
 }
 
+const reactionClass = 'reaction-sidebar-link'
+
 function Reactions() {
-  // console.log('Reactions')
   const all = document.createElement('div')
 
   const issueUrl =
     window.location.origin + window.location.pathname + window.location.search
   // Grabbing all reactions Reactions 👍 🚀 🎉 😄 ❤️ 😕 👎 👀
   const reactions = ['👍', '🚀', '🎉', '😄', '❤️', '😕', '👎', '👀']
+
   Array.from(document.querySelectorAll('.js-comment-reactions-options'))
     .filter((node) =>
       reactions.some((reaction) => node.textContent?.includes(reaction))
@@ -148,6 +164,8 @@ function Reactions() {
           }
         })
       const linkContainer = document.createElement('div')
+      linkContainer.classList.add(reactionClass)
+
       const a = document.createElement('a')
       const linkText = document.createTextNode('  ' + reactions)
       linkContainer.appendChild(a)
@@ -177,12 +195,23 @@ function Reactions() {
 
       all.appendChild(linkContainer)
     })
+
+  getBrowser()
+    .storage.sync.get([DISPLAY])
+    .then((result: { [x: string]: string }) => {
+      const display = (result[DISPLAY] as Display) ?? 'block'
+      const elements = Array.from(
+        document.getElementsByClassName(reactionClass)
+      ) as HTMLDivElement[]
+      for (let element of elements) {
+        element.style.display = display
+      }
+    })
+
   return all
 }
 
 function DiscussionVotes() {
-  // console.log('DiscussionVotes')
-
   const all = document.createElement('div')
   document.querySelectorAll('[data-url]').forEach((discussionComment) => {
     const vote = discussionComment.querySelector('.js-default-vote-count')
@@ -203,11 +232,11 @@ function DiscussionVotes() {
     if (!vote || url.match(/votes$/)) return
     const votes = vote.textContent
     const a = document.createElement('a')
+    a.classList.add(reactionClass)
     const linkText = document.createTextNode('\n⬆️ ' + votes)
     a.appendChild(linkText)
     a.title = url
     a.href = url
-    a.style.display = 'block;'
     all.appendChild(a)
   })
 
@@ -215,7 +244,6 @@ function DiscussionVotes() {
 }
 
 function Credits() {
-  // console.log('Credits')
   const credits = document.createElement('div')
   credits.style.display = 'flex'
   credits.style.gap = '0.5rem'
@@ -247,7 +275,135 @@ function Credits() {
   return credits
 }
 
+// Create the switch container, label, input, and handle elements
+function Switch() {
+  const switchContainer = document.createElement('span')
+  const switchLabel = document.createElement('label')
+  const switchInput = document.createElement('span')
+  switchLabel.appendChild(switchInput)
+  const switchHandle = document.createElement('span')
+  switchHandle.setAttribute('class', 'switch-handle-reactions-layout')
+  switchLabel.appendChild(switchHandle)
+  switchContainer.appendChild(switchLabel)
+
+  // Add CSS styles to the switch container
+  switchContainer.style.display = 'inline-block'
+  switchContainer.style.position = 'relative'
+  switchContainer.style.backgroundColor = cobalt2BackgroundBlue
+  switchContainer.style.borderRadius = '14px'
+  switchContainer.style.width = '30px'
+  switchContainer.style.height = '17px'
+
+  // Add CSS styles to the switch label
+  switchLabel.style.display = 'block'
+  switchLabel.style.position = 'relative'
+  switchLabel.style.width = '100%'
+  switchLabel.style.height = '100%'
+  switchLabel.style.cursor = 'pointer'
+
+  // Add CSS styles to the switch input
+  switchInput.style.display = 'none'
+
+  // Add CSS styles to the switch handle
+  switchHandle.style.display = 'block'
+  switchHandle.style.position = 'absolute'
+  switchHandle.style.top = '1px'
+  switchHandle.style.width = '14px'
+  switchHandle.style.height = '14px'
+  switchHandle.style.borderRadius = '14px'
+  switchHandle.style.backgroundColor = wesBosYellow
+  switchHandle.style.boxShadow = '0 2px 4px 0 rgba(0, 0, 0, 0.1)'
+  switchHandle.style.transition = 'left 0.2s ease-in-out'
+
+  const toggle = (value: boolean) => {
+    switchHandle.style.left = value ? '12px' : '2px'
+  }
+
+  getBrowser()
+    .storage.sync.get([DISPLAY])
+    .then((result) => {
+      toggle(result[DISPLAY] === 'inline-block')
+    })
+
+  // Add an event listener to the switch
+  switchContainer.addEventListener('click', async function changeSwitch() {
+    // Get Layout from storage and change it to the opposite (either block or inline-block)
+    const settings = await getBrowser().storage.sync.get([DISPLAY])
+    const display: Display =
+      settings[DISPLAY] === 'inline-block' ? 'block' : 'inline-block'
+    getBrowser().storage.sync.set({ [DISPLAY]: display })
+  })
+
+  getBrowser().storage.onChanged.addListener(function (changes, _namespace) {
+    for (const key in changes) {
+      const storageChange = changes[key]
+      if (key === DISPLAY) {
+        toggle(storageChange.newValue === 'inline-block')
+      }
+    }
+  })
+
+  return switchContainer
+}
+
+getBrowser().storage.onChanged.addListener(function (changes) {
+  for (let key in changes) {
+    if (key === DISPLAY) {
+      const display =
+        changes[key].newValue === 'block' ? 'block' : 'inline-block'
+      const elements = Array.from(
+        document.getElementsByClassName(reactionClass)
+      ) as HTMLDivElement[]
+      for (let element of elements) {
+        element.style.display = display
+      }
+    }
+  }
+})
+
+function SvgBlockIcon() {
+  const svg = `
+    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect width="14" height="14" />
+      <line x1="1.5" y1="1.5" x2="3.5" y2="1.5" stroke=${wesBosYellow} stroke-linecap="round"/>
+      <line x1="1.5" y1="3.5" x2="3.5" y2="3.5" stroke=${wesBosYellow} stroke-linecap="round"/>
+      <line x1="1.5" y1="5.5" x2="7.5" y2="5.5" stroke=${wesBosYellow} stroke-linecap="round"/>
+      <line x1="1.5" y1="7.5" x2="3.5" y2="7.5" stroke=${wesBosYellow} stroke-linecap="round"/>
+      <line x1="1.5" y1="9.5" x2="5.5" y2="9.5" stroke=${wesBosYellow} stroke-linecap="round"/>
+      <line x1="1.5" y1="11.5" x2="3.5" y2="11.5" stroke=${wesBosYellow} stroke-linecap="round"/>
+    </svg>
+  `
+  const span = document.createElement('span')
+  span.innerHTML = svg
+
+  return span
+}
+
+function SvgInlineBlockIcon() {
+  const svg = `
+    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect width="14" height="14" />
+      <line x1="1.5" y1="1.5" x2="3.5" y2="1.5" stroke=${wesBosYellow} stroke-linecap="round"/>
+      <line x1="5.5" y1="1.5" x2="7.5" y2="1.5" stroke=${wesBosYellow} stroke-linecap="round"/>
+      <line x1="1.5" y1="3.5" x2="7.5" y2="3.5" stroke=${wesBosYellow} stroke-linecap="round"/>
+      <line x1="9.5" y1="3.5" x2="11.5" y2="3.5" stroke=${wesBosYellow} stroke-linecap="round"/>
+      <line x1="1.5" y1="5.5" x2="5.5" y2="5.5" stroke=${wesBosYellow} stroke-linecap="round"/>
+      <line x1="7.5" y1="5.5" x2="9.5" y2="5.5" stroke=${wesBosYellow} stroke-linecap="round"/>
+    </svg>
+  `
+  const span = document.createElement('span')
+  span.innerHTML = svg
+
+  return span
+}
+
 declare interface Element {
   dataset: DOMStringMap
   name: string
+}
+
+type Display = 'block' | 'inline-block'
+
+type Settings = {
+  [DISPLAY]: Display
 }
