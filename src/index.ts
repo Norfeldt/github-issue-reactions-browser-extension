e80886f27639784e5f9f58d4e7d02d46d1d9c3e4
Lasse Nørfeldt <lasse@norfeldt.com>
2023-02-14 19:41:24 +0100

    💄 ♻️ refactor discussions votes

--- src/index.ts
+++ src/index.ts
@@ -217,7 +217,14 @@ function Reactions() {
 function DiscussionVotes() {
   const all = document.createElement('div')
   document.querySelectorAll('[data-url]').forEach((discussionComment) => {
-    const vote = discussionComment.querySelector('.js-default-vote-count')
+    const defaultVote = discussionComment.querySelector(
+      '.js-default-vote-count'
+    )
+    const upvotedVote = discussionComment.querySelector(
+      '.js-upvoted-vote-count'
+    )
+    const isUpvote = !!discussionComment.querySelector('[data-upvoted="true"]')
+    const vote = isUpvote ? upvotedVote : defaultVote
     let url = discussionComment.dataset?.url?.replace(
       '/comments/',
       '#discussioncomment-'
@@ -235,12 +242,20 @@ function DiscussionVotes() {
     if (!vote || url.match(/votes$/)) return
     const votes = vote.textContent
     const a = document.createElement('a')
-    a.classList.add(reactionClass)
-    const linkText = document.createTextNode('\n⬆️ ' + votes)
+    const linkText = document.createTextNode(
+      `\n⬆️ ${votes}${isUpvote ? ' 🫵' : ''}`
+    )
     a.appendChild(linkText)
     a.title = url
     a.href = url
-    all.appendChild(a)
+    a.style.border = '1px solid var(--color-border-default, #d2dff0)'
+    a.style.borderRadius = '100px'
+    a.style.padding = '2px 7px'
+    a.style.margin = '0.5rem 0'
+    const linkContainer = document.createElement('div')
+    linkContainer.appendChild(a)
+    linkContainer.classList.add(reactionClass)
+    all.appendChild(linkContainer)
   })
 
   return all
